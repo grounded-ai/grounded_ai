f1ed0bef2d7f7156b23d1ac185408f731c6d7df5
jlonge4 <josh.longenecker1@gmail.com>
2024-06-15 21:51:15 -0400

    best practices

--- grounded_ai/evaluators/hallucination_evaluator.py
+++ grounded_ai/evaluators/hallucination_evaluator.py
@@ -1,5 +1,3 @@
-from peft import PeftModel, PeftConfig
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from transformers import pipeline
 import torch
 
@@ -10,10 +8,8 @@ class HallucinationEvaluator:
 
     Example Usage:
     ```python
-    base_model_id = "microsoft/Phi-3-mini-4k-instruct"
-    groundedai_eval_id = "grounded-ai/phi3-hallucination-judge"
-    evaluator = HallucinationEvaluator(base_model_id, groundedai_eval_id, quantization=True)
-    evaluator.load_model(base_model_id, groundedai_eval_id)
+    evaluator = HallucinationEvaluator(quantization=True)
+    evaluator.warmup()
     data = [
         ['Based on the following <context>Walrus are the largest mammal</context> answer the question <query> What is the best PC?</query>', 'The best PC is the mac'],
         ['What is the color of an apple', "Apples are usually red or green"],
@@ -47,81 +43,18 @@ class HallucinationEvaluator:
     ```
     """
 
-    def __init__(
-        self, base_model_id: str, groundedai_eval_id: str, quantization: bool = False
-    ):
-        self.base_model_id: str = base_model_id
-        self.groundedai_eval_id: str = groundedai_eval_id
-        self.model = None
-        self.tokenizer = None
-        self.quantization: bool = quantization
-
-    def load_model(self, base_model_id: str, groundedai_eval_id: str):
-        if torch.cuda.is_bf16_supported():
-            compute_dtype = torch.bfloat16
-            attn_implementation = "flash_attention_2"
-            # If bfloat16 is not supported, 'compute_dtype' is set to 'torch.float16' and 'attn_implementation' is set to 'sdpa'.
-        else:
-            compute_dtype = torch.float16
-            attn_implementation = "sdpa"
-
-        config = PeftConfig.from_pretrained(groundedai_eval_id)
-        tokenizer = AutoTokenizer.from_pretrained(base_model_id)
-        if self.quantization:
-            bnb_config = BitsAndBytesConfig(
-                load_in_8bit=True,
-            )
-            base_model = AutoModelForCausalLM.from_pretrained(
-                base_model_id,
-                attn_implementation=attn_implementation,
-                torch_dtype=compute_dtype,
-                quantization_config=bnb_config,
-            )
-            model_peft = PeftModel.from_pretrained(
-                base_model, groundedai_eval_id, config=config
-            )
-            merged_model = model_peft.merge_and_unload()
-        else:
-            base_model = AutoModelForCausalLM.from_pretrained(
-                base_model_id,
-                attn_implementation=attn_implementation,
-                torch_dtype=compute_dtype,
-            )
-            model_peft = PeftModel.from_pretrained(
-                base_model, groundedai_eval_id, config=config
-            )
-
-            merged_model = model_peft.merge_and_unload()
-            merged_model.to("cuda")
-
-        self.model = merged_model
-        self.tokenizer = tokenizer
-
     def format_func(self, query: str, response: str, reference: str = None) -> str:
-        # TODO implement promt hub and optionally pass in user defined prompt
-        if reference is None:
-            prompt = f"""Your job is to evaluate whether a machine learning model has hallucinated or not.
-            A hallucination occurs when the response is coherent but factually incorrect or nonsensical
-            outputs that are not grounded in the provided context.
-            You are given the following information:
-                ####INFO####
-                [User Input]: {query}
-                [Model Response]: {response}
-                ####END INFO####
-                Based on the information provided is the model output a hallucination? Respond with only "yes" or "no"
-                """
-        else:
-            prompt = f"""Your job is to evaluate whether a machine learning model has hallucinated or not.
-              A hallucination occurs when the response is coherent but factually incorrect or nonsensical
-              outputs that are not grounded in the provided context.
-              You are given the following information:
-                  ####INFO####
-                  [Knowledge]: {reference}
-                  [User Input]: {query}
-                  [Model Response]: {response}
-                  ####END INFO####
-                  Based on the information provided is the model output a hallucination? Respond with only "yes" or "no"
-                """
+        knowledge_line = f"[Knowledge]: {reference}\n" if reference is not None else ""
+        prompt = f"""Your job is to evaluate whether a machine learning model has hallucinated or not.
+    A hallucination occurs when the response is coherent but factually incorrect or nonsensical
+    outputs that are not grounded in the provided context.
+    You are given the following information:
+        ####INFO####
+        {knowledge_line}[User Input]: {query}
+        [Model Response]: {response}
+        ####END INFO####
+    Based on the information provided is the model output a hallucination? Respond with only "yes" or "no"
+    """
         return prompt
 
     def run_model(self, query: str, response: str, reference: str = None) -> str:
