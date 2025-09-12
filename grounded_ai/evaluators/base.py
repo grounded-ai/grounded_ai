f2677b5fca5e1d34f323db750c8c20cb9a94cf8e
Jlonge4 <josh.longenecker1@gmail.com>
2025-09-11 20:00:36 -0400

    cleanup and updeate base.py

--- grounded_ai/evaluators/base.py
+++ grounded_ai/evaluators/base.py
@@ -2,19 +2,28 @@ import os
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Optional
+from enum import Enum
 
 import torch
 from peft import PeftConfig, PeftModel
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
-BASE_MODEL_ID = os.getenv("BASE_MODEL_ID", "microsoft/Phi-3-mini-4k-instruct")
+BASE_MODEL_ID = os.getenv("BASE_MODEL_ID", "microsoft/Phi-3.5-mini-instruct")
 
+class EvalMode(str, Enum):
+    ANY = "ANY"
+    TOXICITY = "TOXICITY"
+    RAG_RELEVANCE = "RAG_RELEVANCE"
+    HALLUCINATION = "HALLUCINATION"
 
 @dataclass
 class BaseEvaluator(ABC):
-    base_model: Optional[AutoModelForCausalLM] = None
-    tokenizer: Optional[AutoTokenizer] = None
+    base_model: AutoModelForCausalLM = None
+    tokenizer: AutoTokenizer = None
     merged_model: Optional[PeftModel] = None
+    use_peft: Optional[bool] = True
+    quantization: Optional[bool] = False
+    eval_mode: EvalMode = EvalMode.ANY
 
     @property
     @abstractmethod
@@ -23,22 +32,17 @@ class BaseEvaluator(ABC):
 
     @property
     @abstractmethod
-    def quantization(self) -> bool:
-        ...
-
-    @property
-    @abstractmethod
     def base_prompt(self) -> str:
         ...
 
     def warmup(self):
-        """Warmup the model by loading it and merging the adapter"""
+        """Warmup the model by loading it and merging the adapter if necessary."""
         self.load_model()
-        self.merge_adapter(self.groundedai_eval_id)
+        if self.use_peft:
+            self.merge_adapter(self.groundedai_eval_id)
 
     def load_model(self):
         """Loads the base model with or without quantization."""
-        # TODO Error handling for model loading could be added here
         compute_dtype = (
             torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float16
         )
@@ -60,7 +64,6 @@ class BaseEvaluator(ABC):
 
     def merge_adapter(self, groundedai_eval_id: str):
         """Merges the PEFT adapter into the base model."""
-        # TODO Error handling for adapter merging could be added here
         config = PeftConfig.from_pretrained(groundedai_eval_id)
         model_peft = PeftModel.from_pretrained(
             self.base_model, groundedai_eval_id, config=config
