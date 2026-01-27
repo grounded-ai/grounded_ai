ea7715556d03e10d942f36319ba38910f3f9a1da
Jlonge4 <josh.longenecker1@gmail.com>
2026-01-27 18:21:05 -0500

    Revert "pass kwargs to backend"
    This reverts commit 5d959c68dd512b2a54bfdcc49a539ad6266247f8.

--- grounded_ai/__init__.py
+++ grounded_ai/__init__.py
@@ -71,7 +71,7 @@ class Evaluator:
         elif input_data is None:
             input_data = EvaluationInput(**kwargs)
 
-        return self.backend.evaluate(input_data, output_schema=output_schema, **kwargs)
+        return self.backend.evaluate(input_data, output_schema=output_schema)
 
 
 __all__ = ["Evaluator", "EvaluationInput", "EvaluationOutput", "EvaluationError"]
